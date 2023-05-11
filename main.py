571aabfcb8a0cd23aacc4316aeb8c42a0cc7ad24
Axello10 <alainaxelmataruka@gmail.com>
2023-05-11 13:19:49 +0200

    Creation d'une liste  de 10 elements

---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__== '__main__':
+    # Q1
+    # Creation d'une liste  de 10 element  de type chaine de caracteres
+    animaux = ["chameau", "chien", "lion", "oiseau", "poisson", "chevre", "vache", "cheval", "moustique", "canard"]
+    
+    # 1- Affichage des elements de la liste
+    print("1) Affichage des elements de la liste")
+    print("--------------------------------------------------")
+    print(animaux)
+    print("--------------------------------------------------\n")
